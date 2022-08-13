62d251425c12652a23d4a04514e43c3805537e9e
ohtsuka takahiro <t.o.0526@gmail.com>
2022-08-13 16:22:26 +0900

    create test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Article.create(
+  [
+    { title: "タイトル1", content: "内容1" },
+    { title: "タイトル2", content: "内容2" },
+    { title: "タイトル3", content: "内容3" },
+  ]
+)
